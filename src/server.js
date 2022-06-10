fc47c94025e8178513e8fb2320b6916f4258a287
PauloHenriqueMagno <paulohm2309@gmail.com>
2022-06-10 19:20:55 -0300

    feat: change PORT

--- src/server.js
+++ src/server.js
@@ -4,7 +4,10 @@ import app from "./app.js"
 
 dotenv.config()
 
-const PORT = process.env.PORT ?? 3000;
+// Port que o servidor ira rodar por padrão está como 8080
+// Para alterar é necessario colocar a variavel de ambiente PORT
+// no arquivo .env como ngo exemplo do arquivo .env.example
+const PORT = process.env.PORT ?? 8080;
 
 app.listen(PORT, () => {
   console.log(`Server running in http://localhost:${PORT}`);
