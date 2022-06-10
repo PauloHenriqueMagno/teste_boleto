2d376e07fb390414a3c2ff4123ff970a838fe48e
PauloHenriqueMagno <paulohm2309@gmail.com>
2022-06-10 19:16:19 -0300

    feat(routes): add routes and error handler

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,19 @@
 import { Router } from "express";
 
+import handleError from "../utils/handleError.js";
+
+import boletoRouter from "./boleto/index.js";
+import conceRouter from "./concessionaria/index.js";
+
 const router = Router();
+
+// Adiciona as rotas para o router 
+router.use('/boleto/concessionaria/', conceRouter);
+router.use('/boleto', boletoRouter);
+
+// Recebe a mensagem de Error
+router.use((err, _, res, __) => {
+  handleError(err, res);
+});
 
 export default router;
